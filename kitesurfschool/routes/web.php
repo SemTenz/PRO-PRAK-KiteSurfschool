f832a7f7585eb62cb98e87946f5e799805bcb4fc
Steff Brouwer <sbrouwer05@gmail.com>
2024-03-13 13:08:31 +0100

    Navbar update

--- kitesurfschool/routes/web.php
+++ kitesurfschool/routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', function () {
-    return view('home/home'); 
+    return view('home/home');
 })->name('home');
 
 // Route::get('/dashboard', function () {
